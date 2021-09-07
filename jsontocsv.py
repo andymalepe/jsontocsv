5b4c045b12afa8dcc827153c88301eecadfb51f0
Kagiso Malepe <kagiso@cagegroup.co.za>
2021-09-07 21:39:12 +0200

    Select specific column criteria

--- jsontocsv.py
+++ jsontocsv.py
@@ -21,8 +21,14 @@ if len(sys.argv) >= 2:
         #Output all file columns
         #print(pandasDataFrame.columns)
         #Keep specific columns
-        filteredDataFrame=pandasDataFrame.filter(items=['eventid', 'timestamp', 'src_ip', 'dst_ip', 'session', 'duration', 'messagee'])
+        filteredDataFrame=pandasDataFrame.filter(items=['eventid', 'timestamp', 'src_ip', 'dst_ip', 'session', 'duration', 'message'])
         #Convert Pfiltered dataframe to csv
         filteredDataFrame.to_csv(dumpFilename+'.csv', index=False)
+        
+        df=pandas.read_csv(dumpFilename+'.csv')
+        newdataset=df.fillna(method='ffill')
+        blah=newdataset.loc[newdataset['message'].str.contains('login attempt')]
+        print(blah)
+        
     else: print('Input file is not a JSON file')
 else: print('No terminal input json file')
